67149ba66eb31c627f416287dcd68de3fa2e99db
unknown <amrfwub@gmail.com>
2021-12-29 21:54:42 +0500

    ten(r)

--- core/src/com/flappybird/game/FlappyBird.java
+++ core/src/com/flappybird/game/FlappyBird.java
@@ -3,11 +3,11 @@ package com.flappybird.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
