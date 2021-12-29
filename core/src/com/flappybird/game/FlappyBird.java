d7b644deaf74de9030e5ef67bf511c4fb13089c8
unknown <amrfwub@gmail.com>
2021-12-29 17:43:30 +0500

    second(r)

--- core/src/com/flappybird/game/FlappyBird.java
+++ core/src/com/flappybird/game/FlappyBird.java
@@ -8,21 +8,31 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture background,bird;
+	Texture background;
+	Texture[] birds;
+    int flapState=0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background=new Texture("bg.png");
-		bird=new Texture("bird1.png");
+		birds=new Texture[2];
+		birds[0]=new Texture("bird1.png");
+		birds[1]=new Texture("bird2.png");
 
 	}
 
 	@Override
 	public void render () {
+		if(flapState==0){
+			flapState=1;
+		}
+		else{
+			flapState=0;
+		}
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(bird,Gdx.graphics.getWidth()/2-bird.getWidth()/2,Gdx.graphics.getHeight()/2-bird.getHeight()/2);
+		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2-birds[flapState].getWidth()/2,Gdx.graphics.getHeight()/2-birds.getHeight()/2);
 		batch.end();
 	}
 	
